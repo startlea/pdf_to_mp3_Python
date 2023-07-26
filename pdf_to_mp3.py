9402db616bdddfb27e077a4d7c03150d06b85134
startlea <eos1984kowalska@gmail.com>
2023-07-26 20:29:50 +0200

    sound pdf_to_mp3.py

--- pdf_to_mp3.py
+++ pdf_to_mp3.py
@@ -1,19 +1,20 @@
-import pyttsx3,PyPDF2
+import pyttsx3, PyPDF2
 from PyPDF2 import PdfReader
 
-pdfreader = PdfReader("book.pdf") #You can also place "input("What is the path of the file with name.pdf ") in the "book.pdf" place so it would take file full path
+pdfreader = PdfReader("book.pdf")
+# You can also place "input("What is the path of the file with name.pdf ") in the "book.pdf" place so it would take file full path
 page_num = len(pdfreader.pages)
-engine = pyttsx3.init()
+sound = pyttsx3.init()
 text = ""
 
 for page_num in range(0, page_num):
     page = pdfreader.pages[page_num]
     text += page.extract_text((0, page_num)) + "\n"
 
-print(text)  
+print(text)
 
-engine.say(text)
-engine.save_to_file(text, 'story.mp3')
-engine.runAndWait()
+sound.say(text)
+sound.save_to_file(text, "story.mp3")
+sound.runAndWait()
 
-engine.stop()
+sound.stop()
